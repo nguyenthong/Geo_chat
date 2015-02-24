73cc438c46c70973c048662012fc83066a1cb023
nguyenthong1992 <thongprogramming@gmail.com>
2015-02-25 00:02:28 +0200

    Updateing gulp.js with adding gulp-connect module

--- gulpfile.js
+++ gulpfile.js
@@ -6,13 +6,16 @@ var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sh = require('shelljs');
-
+var connect = require('gulp-connect');
+ 
 var paths = {
-  sass: ['./scss/**/*.scss']
+  sass: ['./scss/**/*.scss'],
+  www : ['www/**/*.*']
 };
-
+ 
 gulp.task('default', ['sass']);
-
+gulp.task('serve', ['connect', 'watch']);
+ 
 gulp.task('sass', function(done) {
   gulp.src('./scss/ionic.app.scss')
     .pipe(sass())
@@ -24,18 +27,24 @@ gulp.task('sass', function(done) {
     .pipe(gulp.dest('./www/css/'))
     .on('end', done);
 });
-
+ 
+gulp.task('reload', function () {
+  return gulp.src(['www/index.html'])
+    .pipe(connect.reload());
+});
+ 
 gulp.task('watch', function() {
   gulp.watch(paths.sass, ['sass']);
+  gulp.watch([paths.www], ['reload']);
 });
-
+ 
 gulp.task('install', ['git-check'], function() {
   return bower.commands.install()
     .on('log', function(data) {
       gutil.log('bower', gutil.colors.cyan(data.id), data.message);
     });
 });
-
+ 
 gulp.task('git-check', function(done) {
   if (!sh.which('git')) {
     console.log(
@@ -47,4 +56,12 @@ gulp.task('git-check', function(done) {
     process.exit(1);
   }
   done();
+});
+ 
+gulp.task('connect', function() {
+  connect.server({
+    root: 'www',
+    port: '1881',
+    livereload: true
+  });
 });
