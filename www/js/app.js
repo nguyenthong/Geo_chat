2a981a03d88de4215d097b68c53fb0435423c234
nguyenthong1992 <thongprogramming@gmail.com>
2015-02-26 01:21:03 +0200

    adding hammer and firebase dependency

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('geo_chat', ['ionic', 'geo_chat.controllers', 'geo_chat.services'])
+angular.module('geo_chat', ['ionic', 'geo_chat.controllers', 'geo_chat.services', 'firebase', 'hmTouchEvents'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
