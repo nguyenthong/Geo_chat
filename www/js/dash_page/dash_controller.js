61c2543dc9dead2bc39821e25ccb67134e6bc056
nguyenthong1992 <thongprogramming@gmail.com>
2015-03-12 00:21:08 +0200

    making local location work in DashCtrl

--- www/js/dash_page/dash_controller.js
+++ www/js/dash_page/dash_controller.js
@@ -1,9 +1,9 @@
 (function (angular) {
   "use strict";
   angular.module('geo_chat')
-   .controller('DashCtrl', ['$scope', '$rootScope', 'uiGmapGoogleMapApi', 'GetProfileService','$state', DashCtrl]);
+   .controller('DashCtrl',['$scope', '$rootScope', '$cordovaGeolocation','uiGmapGoogleMapApi', 'GetProfileService','$timeout', DashCtrl]);
 
-    function DashCtrl($scope,$rootScope, uiGmapGoogleMapApi, GetProfileService) {
+    function DashCtrl($scope,$rootScope, $cordovaGeolocation, uiGmapGoogleMapApi, GetProfileService, $timeout) {
       GetProfileService.userProfile()
         .then(getUserSuccess, getUserError);
 
@@ -18,7 +18,27 @@
       // Do stuff with your $scope.
       // Note: Some of the directives require at least something to be defined originally!
       // e.g. $scope.markers = []
-      $scope.map = {center: {latitude: 51.219053, longitude: 4.404418 }, zoom: 14 };
+      var posOptions = {timeout: 1000, enableHighAccuracy: true};
+      $cordovaGeolocation
+        .getCurrentPosition(posOptions)
+        .then(getLocationSuccess, getLocationError);
+
+        function getLocationSuccess(position) {
+          $timeout(function () {
+            $scope.map = {
+            center: {
+              latitude: position.coords.latitude,
+              longitude:  position.coords.longitude
+            },
+            zoom: 14
+          };
+          }, 0);
+        }
+
+        function getLocationError(err) {
+          console.log(err);
+        }
+
       $scope.options = {scrollwheel: false};
       // uiGmapGoogleMapApi is a promise.
       // The "then" callback function provides the google.maps object.
