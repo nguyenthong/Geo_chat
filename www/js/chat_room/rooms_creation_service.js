a44dca8b6d3288d25161b7b6dc3b3f24816d28aa
nguyenthong1992 <thongprogramming@gmail.com>
2015-03-15 23:24:26 +0200

    adding the roomID as reff for member and messages database

--- www/js/chat_room/rooms_creation_service.js
+++ www/js/chat_room/rooms_creation_service.js
@@ -7,44 +7,70 @@
     .service('RoomService', ['ROOMURL', 'MSGURL', 'MEMBERGURL', '$q', '$firebase', RoomsService]);
     
   function RoomsService( ROOMURL, MSGURL, MEMBERGURL, $q, $firebase) {
-      var roomRef = new Firebase(ROOMURL);
-      //todo fix this, not query all the room
-      var fireRoomArray = $firebase(roomRef).$asArray();
+    var roomRef = new Firebase(ROOMURL);
+    var geoFire = new GeoFire(roomRef);
+    var geoRef = geoFire.ref();
 
-      //var geoRoom = new GeoFire(ROOMURL);
-      ////var geoRoomRef = geoRoom.ref();
+    //todo fix this, not query all the room
+    //var fireRoomArray = $firebase(roomRef).$asArray();
 
-      var messageRef = new Firebase(MSGURL);
+    //var geoRoom = new GeoFire(ROOMURL);
+    ////var geoRoomRef = geoRoom.ref();
 
-      var memberRef = new Firebase(MEMBERGURL);
-      var fireMember = $firebase(memberRef).$asArray();
+    var messageRef = new Firebase(MSGURL);
 
-      return {
-        //todo make this only get the room inside radius
-        //childAdded: function childAdded(cb) {
-        //  fireRoomArray.once('child_added', function (data) {
-        //    var val = data.snapshot.value;
-        //    cb.call(this, {
-        //      room_name : val.room_name,
-        //      type      : val.type,
-        //      location  : val.location,
-        //      radius    : val.radius,
-        //      name      : data.snapshot.name
-        //    });
-        //  });
-        //},
-        //todo everytime create new room, automatically added roomID to the messages, members objects
-        createRoom: function CreateRoom(newRoom) {
-          return roomRef.push(newRoom);
-        },
-        //todo function to add user to the room
-        get: function GetRooms(roomId) {
-          return $firebase(roomRef.child(roomId)).$asObject();
-        },
-        delete: function (room) {
-          return fireRoomArray.$remove(room);
-        }
-      };
+    var memberRef = new Firebase(MEMBERGURL);
+    var fireMember = $firebase(memberRef).$asArray();
+
+    function onComplete(data, snapshot) {
+      if (data === Error){
+        console.log(Error);
+      }else{
+        //todo making ionic show success message
+        var successMes = "Your Room is created";
+        console.log(successMes);
+      }
     }
+
+    return {
+      //todo make this only get the room inside radius
+      //childAdded: function childAdded(cb) {
+      //  fireRoomArray.once('child_added', function (data) {
+      //    var val = data.snapshot.value;
+      //    cb.call(this, {
+      //      room_name : val.room_name,
+      //      type      : val.type,
+      //      location  : val.location,
+      //      radius    : val.radius,
+      //      name      : data.snapshot.name
+      //    });
+      //  });
+      //},
+      //todo everytime create new room, automatically added roomID to the messages, members objects
+      createRoom: function CreateRoom(newRoom) {
+        var newRoomRef = roomRef.push(newRoom, onComplete);
+        var newRoomID = newRoomRef.key();
+        //using newRoomID to set ID for messages, locations, members
+        memberRef.set({
+          roomID: newRoomRef.key(),
+          info:{
+          }
+        });
+        messageRef.set({
+          roomID: newRoomRef.key(),
+          info:{
+          }
+        });
+        return console.log(newRoomID);
+      },
+      //todo function to add user to the room
+      get: function GetRooms(roomId) {
+        return $firebase(roomRef.child(roomId)).$asObject();
+      },
+      delete: function (room) {
+      // todo adding remove method
+      }
+    };
+  }
 
 })(window.angular);
