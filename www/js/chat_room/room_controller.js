f569245c3c88e95c9f69206de46d9b63e0d14c9a
nguyenthong1992 <thongprogramming@gmail.com>
2015-03-17 01:29:52 +0200

    adding the range to the form and message error

--- www/js/chat_room/room_controller.js
+++ www/js/chat_room/room_controller.js
@@ -4,6 +4,11 @@
   angular.module('geo_chat')
     .controller('RoomCtrl', ['$scope', '$cordovaGeolocation', 'uiGmapGoogleMapApi', '$timeout', 'RoomService', RoomCtrl]);
   function RoomCtrl($scope, $cordovaGeolocation, uiGmapGoogleMapApi, $timeout, RoomService) {
+    var defaultForm = {
+        name: "",
+        private: "",
+        range: ""
+      };
 
     var posOptions = {timeout: 1000, enableHighAccuracy: true};
       $cordovaGeolocation
@@ -24,15 +29,41 @@
         console.log(err);
       }
 
-    setTimeout(getLocationSuccess, 1000);
+    setTimeout(getLocationSuccess, 0);
 
     //Create Room
+
     $scope.createRoom = function (newRoom) {
-      var pushRoomData = {
-        name: newRoom.name,
-        location: [$scope.map.center.latitude, $scope.map.center.longitude]
-      };
-      RoomService.createRoom(pushRoomData);
+      if (newRoom === undefined){
+        $scope.errorMes = "*Room name is required";
+      }
+      else{
+        switch (newRoom.private){
+          case undefined:
+            var pushRoomData = {
+              name: newRoom.name,
+              private: newRoom.private,
+              range: newRoom.range,
+              location: [$scope.map.center.latitude, $scope.map.center.longitude]
+            };
+            pushRoomData.private = false;
+            pushRoomData.range = 0;
+            RoomService.createRoom(pushRoomData);
+            break;
+          case !undefined:
+            var pushRoomData = {
+              name: newRoom.name,
+              private: newRoom.private,
+              range: newRoom.range,
+              location: [$scope.map.center.latitude, $scope.map.center.longitude]
+            };
+            pushRoomData.private = newRoom.private;
+            pushRoomData.range = newRoom.range;
+            RoomService.createRoom(pushRoomData);
+            break;
+        }
+      }
+
     };
 
     $scope.options = {scrollwheel: false};
