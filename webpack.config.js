137893fd410d6a521dcd79513e8e993580ca36dd
Nguyen Thong <nguyenthong1992@hotmail.com>
2015-05-12 00:27:26 +0300

    adding webpack config with babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,26 @@
+'use strict';
+var webpack = require('webpack'),
+path = require('path');
+// PATHS
+var PATHS = {
+  app: __dirname + '/www',
+  bower: __dirname + '/www/lib'
+};
 // webpack.config.js
 module.exports = {
-  entry: './main.js',
+  context: PATHS.app,
+  entry: {
+    app: './index.js'
+  },
   output: {
+    path: PATHS.app,
     filename: 'bundle.js'
-  }
+  },
+  loaders: [
+    {
+      test: /\.js$/,
+      loader: 'ng-annotate!babel',
+      exclude: /node_modules|bower_components/
+    }
+  ]
 };
